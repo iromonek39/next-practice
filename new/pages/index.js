7c7b367dc91502c29210350fa93289743f5f3cc4
iromonek39 <kentarou.omori0309@gmail.com>
2022-09-23 23:44:36 +0900

    2-6

--- new/pages/index.js
+++ new/pages/index.js
@@ -1,9 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Footer from '@/components/Footer'
-import Header from '@/components/Header'
 import Hero from '@/components/Hero'
 import styles from '../styles/Home.module.css'
+import Layout from '@/components/Layout'
 
 const EachPost = () => {
   return (
@@ -18,11 +17,7 @@ const EachPost = () => {
 export default function Home() {
   return (
     <>
-      <Header />
-      <main>
-        <Hero />
-      </main>
-      <Footer />
+      <Hero />
     </>
   )
 }
